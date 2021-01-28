5849f1bec73e39b271f6c1bcd07378907cfc573a
shoblik <shoblik@yahoo.com>
2021-01-27 16:47:25 -0800

    block drops work

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     animation.init();
 })
 
+$(window).on('scroll',function() {
+    animation.rain = true;
+    $(window).off();
+});
+
 var animation = {
     frameCount: 0,
     squareCount: 0,
@@ -9,6 +14,10 @@ var animation = {
     containerHeight: null,
     animate: true,
     createSquare: true,
+    rain: false,
+    rainDrops: [
+
+    ],
 
     init: function() {
         animation.containerWidth = $('#target').innerWidth();
@@ -20,28 +29,30 @@ var animation = {
     },
 
     frame: function() {
+        animation.frameCount++;
+
         if (animation.createSquare) {
             animation.createRandomSquare();
         }
 
+        if (animation.rain) {
+            animation.createRainDrop();
+        }
+
         if (animation.animate) {
-            var id = requestAnimationFrame(animation.frame);
+            requestAnimationFrame(animation.frame);
         }
     },
 
     createRandomSquare: function() {
-        animation.squareCount++;
+            animation.squareCount++;
 
-        if (animation.squareCount < 2500) {
+        if (animation.squareCount < 1500) {
             var coords = animation.getRandomPosition();
 
-            if (animation.squareCount % 30 === 0) {
-                coords.x = 0;
-            }
+            if (animation.squareCount % 30 === 0) coords.x = 0;
 
-            if (animation.squareCount % 31 === 0) {
-                coords.y = 0;
-            }
+            if (animation.squareCount % 31 === 0) coords.y = 0;
 
             var square = $('<square>').addClass('square').css({
                 top: coords.y,
@@ -49,29 +60,67 @@ var animation = {
             });
 
             $('#target').prepend(square);
-        } else {
-            console.log('removing');
-            animation.createSquare = false;
-        }
-    },
 
-    removeASquare: function() {
-        if (animation.squareCount > 0) {
-            $($('.square')[animation.squareCount - 1]).remove();
-            animation.squareCount--;
         } else {
-            setTimeout(function() {
-                animation.createSquare = true;
-                animation.removeSquare = false;
-            }, 2000);
+            console.log('Done with squares');
+            animation.createSquare = false;
         }
     },
 
     getRandomPosition: function() {
         return {
-            x: Math.floor(Math.random() * (animation.containerWidth - 0 + 1)),
-            y: Math.floor(Math.random() * (animation.containerHeight - 0 + 1))
+            x: animation.getRandomNumber(0, animation.containerWidth),
+            y: animation.getRandomNumber(0, animation.containerHeight)
         };
+    },
+
+    getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1));
+    },
+
+    createRainDrop: function() {
+        if (animation.frameCount % 10 === 0) {
+            animation.collectRainWater();
+
+            var randomId = animation.getRandomNumber(10000, 99999);
+
+            var rainDrop = $('<div>').addClass('square').css({
+                top: '-100px',
+                left: animation.getRandomNumber(0, animation.containerWidth),
+            }).attr('id', 'drop' + randomId);
+
+            $('#rainTarget').prepend(rainDrop);
+
+            animation.rainDrops.push('drop' + randomId);
+        }
+
+        animation.moveRainDrops();
+    },
+
+    moveRainDrops: function() {
+        for (var i = 0; i < animation.rainDrops.length; i++) {
+            // get the current y position
+            var nextYPos = Number($('#' + animation.rainDrops[i]).css('top').slice(0, -2)) + Number(15);
+
+            $('#' + animation.rainDrops[i]).css({
+                top: nextYPos + 'px',
+            })
+        }
+    },
+
+    collectRainWater: function() {
+        for (var i = 0; i < animation.rainDrops.length; i++) {
+
+            var nextPos = Number($('#' + animation.rainDrops[i]).css('top').slice(0, -2)) + Number(15);
+
+            if (nextPos > animation.containerHeight) {
+                $('#' + animation.rainDrops[i]).remove();
+                animation.rainDrops.splice(animation.rainDrops.indexOf(animation.rainDrops[i]), 1);
+                console.log('killing in the name of')
+                console.log($('#rainTarget').children().length)
+            }
+
+        }
     }
 }
 
